4ab56d4d696b786879f2f7b98627d00c929b8889
mglujoy <mglujoy@gmail.com>
2021-10-11 19:19:28 -0300

    game() function not working like it should

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -4,25 +4,41 @@ function computerPlay() {
     return choice;
 }
 
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+function game() {
+    while ((scorePlayer + scoreComputer) < 5) {
+        const playerSelection = prompt("Rock, paper or scissors? Choose!", "rock");
+        const computerSelection = computerPlay();
+        return playRound(playerSelection, computerSelection) + "Your Score = " + scorePlayer + ". PC Score = " + scoreComputer + ".";       
+    }    
+}   
+
 function playRound(playerSelection, computerSelection) {
     // your code here!
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
         return "Tie.";
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors") {
+        scorePlayer++;
         return "You Win!";
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") {
+        scorePlayer++;
         return "You Win!";
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock") {
+        scorePlayer++;
         return "You Win!";
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") {
+        scoreComputer++;
         return "You Lose!";
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock") {
+        scoreComputer++;
         return "You Lose!";
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors") {
+        scoreComputer++;
         return "You Lose!";
     } else {console.log("Error, Start Over");}
   } 
   
-  const playerSelection = prompt("Rock, paper or scissors? Choose!", "rock");
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+ 
+  console.log(game());
